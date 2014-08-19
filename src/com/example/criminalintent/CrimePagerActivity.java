027ff887b5e908d131f1617036d36e757a19e21e
kevin shen <kevshin2@kevins-iMac.local>
2014-08-19 16:45:52 -0700

    added ViewPager

---
+++ src/com/example/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,39 @@
+package com.example.criminalintent;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class CrimePagerActivity extends FragmentActivity {
+	private ViewPager mViewPager;
+	private ArrayList<Crime> mCrimes;
+	
+	@Override 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mViewPager = new ViewPager(this);
+		mViewPager.setId(R.id.ViewPager);
+		setContentView(mViewPager);
+		
+		mCrimes = CrimeLab.get(this).getCrimes();
+		
+		FragmentManager fm= getSupportFragmentManager();
+		mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+			@Override
+			public int getCount() {
+				return mCrimes.size();
+			}
+			
+			@Override
+			public Fragment getItem(int pos) {
+				Crime crime = mCrimes.get(pos);
+				return CrimeFragment.newInstance(crime.getId());
+			}
+		});
+	}
+}
